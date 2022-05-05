b8d012f051665c4bf10a7ec59a7e983952433726
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-05-05 04:15:59 +0000

    new python files

---
+++ hello_world.py
@@ -0,0 +1,3 @@
+#!/bin/env python 3.7
+
+print("Hello World, what a wonderful day!") # print greeting statement
---
+++ revised_hello_world.py
@@ -0,0 +1,4 @@
+#!/bin/env python 3.7
+
+print("Hello World, what a wonderful day!") # print greeting statement
+mclark
