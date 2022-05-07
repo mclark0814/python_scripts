77f0b8804d1c9e75323d708635770021a9015a9d
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-05-07 06:23:01 +0000

    Updated the logic for the print function

---
+++ Welcome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.75
+
+# This program will prompt the user for their name and welcome to the first day of their LUIT Cohort
+
+first_name = input("Enter your first name:         ")
+last_name = input("Enter your last name:        ")
+
+print("\nHello", (str(first_name)), (str(last_name)) + "\nWelcome to Level Up In Tech, now let's get started on your next phase!!!")
