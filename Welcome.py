99986cd4529e4afbe6f0fabd664ba9b0906b6f90
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-05-08 03:36:58 +0000

    Updated print function output

--- Welcome.py
+++ Welcome.py
@@ -5,6 +5,9 @@
 first_name = input("Enter your first name:         ") # Prompt the user for their first name
 last_name = input("Enter your last name:        ") # Prompt the user for their last name
 
-print("\nHello", (str(first_name)), (str(last_name)) + "\nWelcome to Level Up In Tech \nLets get started on the next phase of your LIFE!!!") # Display the welcome message and the user's first and last name
+#print("Hello", (str(first_name)), (str(last_name)) + "\nWelcome to Level Up In Tech \nLets get started on the next phase of your LIFE!!!") # Display the welcome message and the user's first and last name
+
+
+print("Hello", first_name, last_name, "welcome to Level Up In Tech. \nLets get started on the next phase of your LIFE!!!") # Display the welcome message and the user's first and last name
 
 # no comment
