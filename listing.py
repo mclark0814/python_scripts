38685b8136e116a96fd76a0590c981de1f190198
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-05-10 06:41:03 +0000

    New and revised python files

---
+++ listing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+
+services_list = []
+
+AWS_Services = input("Please enter the name of an AWS Service: ")
+
+print(services_list)
+
+services_list[len(services_list):] = [x]
+
