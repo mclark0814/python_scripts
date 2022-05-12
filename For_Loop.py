1655cffdff9099ba59f7fae10f60f87759b5eb21
EC2 Default User <ec2-user@ip-172-31-53-25.ec2.internal>
2022-05-12 02:56:13 +0000

    new files

---
+++ For_Loop.py
@@ -0,0 +1,4 @@
+#!/user/bin/env python3.7
+colors = ['blue', 'green', 'red', 'purple'] # List of Colors
+for color in colors:  #For loop used to step through the list named colors
+    print(color)      #print function will display the current color assigned to the color variable
