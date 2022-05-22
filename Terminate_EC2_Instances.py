7b81b5d76c2cff4bd55041a788004123fa4bfc09
EC2 Default User <ec2-user@ip-172-31-63-209.ec2.internal>
2022-05-22 22:58:34 +0000

    New Python scripts

---
+++ Terminate_EC2_Instances.py
@@ -0,0 +1,15 @@
+import boto3
+ec2_client=boto3.client("ec2")
+x=ec2_client.describe_instances()
+data=x["Reservations"]
+
+li=[]
+for instances in data: #(List comprehensive collectng all instances on the account)
+    instance=instances["Instances"]
+    for ids in instance:
+        instance_id=ids["InstanceId"]
+        li.append(instance_id)
+
+#Terminate EC2 instances
+        
+ec2_client.terminate_instances(InstanceIds=li) #Terminate instances that are not protected from accidential termination
