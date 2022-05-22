ccf3dd422c70f94e780911b3e7f8d265efb11db9
EC2 Default User <ec2-user@ip-172-31-63-209.ec2.internal>
2022-05-22 21:59:11 +0000

    New Python Scripts-launching EC2 instances

---
+++ Launch_Multiple_EC2_instances_using_Boto3.py
@@ -0,0 +1,7 @@
+import boto3
+ec2_resource=boto3.resource("ec2")
+ec2_resource.create_instances(ImageId='ami-0022f774911c1d690', 
+    InstanceType='t2.micro', 
+        MaxCount=2,
+            MinCount=2)
+            
