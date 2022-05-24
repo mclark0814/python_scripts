ab0a8d687a9b6e4bea6b42c289ac75b285e28ff9
EC2 Default User <ec2-user@ip-172-31-63-209.ec2.internal>
2022-05-24 00:29:07 +0000

    updated Delete EBS Python script

--- Delete_an_AWS_EBS_Volume_Snapshot_using_boto3.py
+++ Delete_an_AWS_EBS_Volume_Snapshot_using_boto3.py
@@ -1,6 +1,4 @@
 import boto3
-ec2_boto3=boto3.client("ec2")
+ec2_client=boto3.client("ec2")
 
-# Delete AWS EVS spanshot
-ec2_boto3.describe_snapshots(SnapshotIds='snap-04573b7949c0deea7')
-    
+ec2_client.delete_snapshot(SnapshotId='snap-04573b7949c0deea7') # Delete AWS EVS spanshot
